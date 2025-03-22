120e42c56cde4f352814079868f4eddea49f0a12
OMKAR2OO3 <omkarmurugasan@gmail.com>
2025-03-22 14:10:29 +0530

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,8 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
-import AppContextProvider from "./context/Appcontext";
-
+// import AppContextProvider from "./context/AppContext";
+import AppContextProvider from "../src/context/AppContext";
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <AppContextProvider>
